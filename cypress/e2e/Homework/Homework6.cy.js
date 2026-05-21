b92c6d37472085e7594e5a432421ba7ec8d865f7
Tabix <panalien10@gmail.com>
2026-05-21 18:57:40 +0200

    Homework 6 Update

--- cypress/e2e/Homework/Homework6.cy.js
+++ cypress/e2e/Homework/Homework6.cy.js
@@ -14,12 +14,13 @@ describe('Homework Module6', ()=>{
         body: {
             title: 'The coolest title ever',
             body:'Not a lorem ipsum anymore',
+            userId: 24,
         },
             
     }).then((response) => {
         expect(response.status).to.eq(200)
         expect(response.body.title).to.eq('The coolest title ever')
-        expect(response.body.userId).not.to.be.oneOf([2137,67,420]) 
+        expect(response.body.userId).to.eq(24) 
     })
 
     cy.request({
